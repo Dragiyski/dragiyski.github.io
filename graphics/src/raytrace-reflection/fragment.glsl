d9962614f25d693993ab02b763a747a36b41b002
Plamen Dragiyski <plamen@dragiyski.org>
2021-03-18 00:34:41 +0100

    Adding links and experiment with reflection in webgl

---
+++ graphics/src/raytrace-reflection/fragment.glsl
@@ -0,0 +1,252 @@
+#version 300 es
+
+#if GL_ES
+precision highp float;
+precision highp int;
+#endif
+
+#define EPSILON (1.1920928955078125e-7)
+#define MAX_VALUE (1.7014118346046923e+38)
+#define MIN_VALUE (1.401298464324817e-45)
+#define MIN_NORMAL_VALUE (1.1754943508222875e-38)
+
+in vec2 position;
+
+out vec4 fragmentColor;
+
+uniform uvec2 screenSize;
+uniform sampler2D spheres;
+uniform sampler2D lights;
+
+struct Material {
+    float ambient;
+    float diffuse;
+    float specular;
+    float exponent;
+};
+
+struct Sphere {
+    vec3 position;
+    float radius;
+    Material material;
+    vec4 color;
+};
+
+struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+struct Light {
+    vec3 position;
+    vec3 color;
+};
+
+struct RayTrace {
+    vec3 normal;
+    float distance;
+    vec3 hitPoint;
+    vec4 color;
+    Material material;
+};
+
+struct Phong {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+bool isEqual(float a, float b) {
+    if (a == b) {
+        return true;
+    }
+    if (isnan(a) || isnan(b)) {
+        return false;
+    }
+    float diff = abs(a - b);
+    float finAmp = min(abs(a) + abs(b), MAX_VALUE);
+    return diff < max(MIN_VALUE, EPSILON * finAmp);
+}
+
+float length2(vec3 value) {
+    return value.x * value.x + value.y * value.y + value.z * value.z;
+}
+
+RayTrace miss() {
+    RayTrace rayTrace;
+    float zero = 0.0;
+    rayTrace.distance = 1.0 / zero;
+    rayTrace.normal = vec3(0.0, 0.0, 0.0);
+    return rayTrace;
+}
+
+Sphere getSphere(int index) {
+    vec4 tex0 = texelFetch(spheres, ivec2(0, index), 0);
+    vec4 tex1 = texelFetch(spheres, ivec2(1, index), 0);
+    vec4 tex2 = texelFetch(spheres, ivec2(2, index), 0);
+    Sphere sphere;
+    sphere.position = tex0.xyz;
+    sphere.radius = tex0.w;
+    sphere.material.ambient = tex1.x;
+    sphere.material.diffuse = tex1.y;
+    sphere.material.specular = tex1.z;
+    sphere.material.exponent = tex1.w;
+    sphere.color = tex2;
+    return sphere;
+}
+
+Light getLight(int index) {
+    vec4 tex0 = texelFetch(lights, ivec2(0, index), 0);
+    vec4 tex1 = texelFetch(lights, ivec2(1, index), 0);
+    Light light;
+    light.position = tex0.xyz;
+    light.color = tex1.xyz;
+    return light;
+}
+
+RayTrace raytraceSphere(Ray ray, Sphere sphere) {
+    float a = length2(ray.direction);
+    float b = 2.0 * (dot(ray.direction, ray.origin) - dot(ray.direction, sphere.position));
+    float c = length2(ray.origin - sphere.position) - sphere.radius * sphere.radius;
+    float D = b * b - 4.0 * a * c;
+    if (D < 0.0) {
+        return miss();
+    }
+    float x = (-b - sqrt(D)) / (2.0 * a);
+    if (x < 0.0) {
+        x = (-b + sqrt(D)) / (2.0 * a);
+        if (x < 0.0) {
+            return miss();
+        }
+    }
+    RayTrace rayTrace;
+    rayTrace.distance = x;
+    vec3 hitPoint = ray.origin + x * ray.direction;
+    rayTrace.normal = normalize(hitPoint - sphere.position);
+    if (dot(rayTrace.normal, -ray.direction) < 0.0) {
+        rayTrace.normal = -rayTrace.normal;
+    }
+    rayTrace.hitPoint = hitPoint;
+    rayTrace.material = sphere.material;
+    rayTrace.color = sphere.color;
+    return rayTrace;
+}
+
+RayTrace rayTrace(Ray ray) {
+    int objectCount = textureSize(spheres, 0).y;
+    RayTrace trace = miss();
+    for (int i = 0; i < objectCount; ++i) {
+        Sphere sphere = getSphere(i);
+        RayTrace sphereTrace = raytraceSphere(ray, sphere);
+        if (isinf(sphereTrace.distance)) {
+            continue;
+        }
+        if (!isEqual(sphereTrace.distance, 0.0) && sphereTrace.distance < trace.distance) {
+            trace = sphereTrace;
+        }
+    }
+    return trace;
+}
+
+Phong illuminate(RayTrace trace, vec3 viewVector) {
+    Phong phong;
+    phong.ambient = trace.material.ambient * trace.color.xyz;
+    phong.diffuse = vec3(0.0, 0.0, 0.0);
+    phong.specular = vec3(0.0, 0.0, 0.0);
+
+    int lightCount = textureSize(lights, 0).y;
+    for (int i = 0; i < lightCount; ++i) {
+        Light light = getLight(i);
+        vec3 lightVector = light.position - trace.hitPoint;
+        vec3 L = normalize(lightVector);
+        {
+            Ray lightRay;
+            lightRay.origin = trace.hitPoint + 0.5 * L;
+            lightRay.direction = L;
+            RayTrace lightTracer = rayTrace(lightRay);
+            if (lightTracer.distance < length(lightVector)) {
+                continue;
+            }
+        }
+        phong.diffuse += trace.material.diffuse * max(0.0, dot(L, trace.normal)) * trace.color.xyz * light.color;
+        vec3 R = reflect(-L, trace.normal);
+        phong.specular += trace.material.specular * pow(max(0.0, dot(R, viewVector)), trace.material.exponent) * light.color;
+    }
+
+    return phong;
+}
+
+Phong noPhong() {
+    Phong phong;
+    phong.ambient = vec3(0.0, 0.0, 0.0);
+    phong.diffuse = vec3(0.0, 0.0, 0.0);
+    phong.specular = vec3(0.0, 0.0, 0.0);
+    return phong;
+}
+
+Phong reflectRayTrace1(RayTrace previous, vec3 direction) {
+    Ray reflectRay;
+    reflectRay.direction = reflect(direction, previous.normal);
+    reflectRay.origin = previous.hitPoint + 0.5 * reflectRay.direction;
+    RayTrace reflectTrace = rayTrace(reflectRay);
+    if(isinf(reflectTrace.distance)) {
+        return noPhong();
+    }
+    return illuminate(reflectTrace, -reflectRay.direction);
+}
+
+Phong reflectRayTrace2(RayTrace previous, vec3 direction) {
+    Ray reflectRay;
+    reflectRay.direction = reflect(direction, previous.normal);
+    reflectRay.origin = previous.hitPoint + 0.5 * reflectRay.direction;
+    RayTrace reflectTrace = rayTrace(reflectRay);
+    if(isinf(reflectTrace.distance)) {
+        return noPhong();
+    }
+    Phong reflectPhong = illuminate(reflectTrace, -reflectRay.direction);
+    Phong reflectedPhong = reflectRayTrace1(reflectTrace, reflectRay.direction);
+    reflectPhong.specular += reflectTrace.material.specular * (reflectedPhong.ambient + reflectedPhong.diffuse + reflectedPhong.specular);
+    return reflectPhong;
+}
+
+Phong reflectRayTrace3(RayTrace previous, vec3 direction) {
+    Ray reflectRay;
+    reflectRay.direction = reflect(direction, previous.normal);
+    reflectRay.origin = previous.hitPoint + 0.5 * reflectRay.direction;
+    RayTrace reflectTrace = rayTrace(reflectRay);
+    if(isinf(reflectTrace.distance)) {
+        return noPhong();
+    }
+    Phong reflectPhong = illuminate(reflectTrace, -reflectRay.direction);
+    Phong reflectedPhong = reflectRayTrace2(reflectTrace, reflectRay.direction);
+    reflectPhong.specular += reflectTrace.material.specular * (reflectedPhong.ambient + reflectedPhong.diffuse + reflectedPhong.specular);
+    return reflectPhong;
+}
+
+void main() {
+    float screenMinHalf = float(min(screenSize.x, screenSize.y)) * 0.5;
+    float imageRatio = screenMinHalf / 200.0;
+    vec2 screenCenter = vec2(screenSize) * 0.5;
+    vec2 imageOrigin = screenCenter - vec2(screenMinHalf);
+
+    vec2 sceenCoords = (gl_FragCoord.xy - imageOrigin) / imageRatio;
+    Ray ray;
+    ray.origin = vec3(200.0, 200.0, 1000.0);
+    ray.direction = normalize(vec3(sceenCoords, 0.0) - ray.origin);
+
+    int objectCount = textureSize(spheres, 0).y;
+    RayTrace cameraTrace = rayTrace(ray);
+    if (isinf(cameraTrace.distance)) {
+        // discard;
+        fragmentColor = vec4(0.0, 0.0, 0.0, 0.0);
+        return;
+    }
+    gl_FragDepth = cameraTrace.distance;
+
+    Phong cameraPhong = illuminate(cameraTrace, -ray.direction);
+
+    Phong reflectedPhong = reflectRayTrace3(cameraTrace, ray.direction);
+    cameraPhong.specular += cameraTrace.material.specular * (reflectedPhong.ambient + reflectedPhong.diffuse + reflectedPhong.specular);
+
+    fragmentColor = vec4(cameraPhong.ambient + cameraPhong.diffuse + cameraPhong.specular, 1.0);
+}
---
+++ graphics/src/raytrace-reflection/index.js
@@ -0,0 +1,254 @@
+import createProgram from '../../lib/graphics/program.js';
+
+let updateTimer = null;
+
+function update() {
+    if (updateTimer == null) {
+        updateTimer = requestAnimationFrame(paint);
+    }
+}
+
+function wrappedNumber(number, wrap) {
+    return (number % wrap) / wrap;
+}
+
+function bounce(current, radius, height) {
+    return radius + (current * (height - radius));
+}
+
+function paint() {
+    updateTimer = null;
+    const canvas = document.getElementById('screen');
+    const gl = canvas.getContext('webgl2');
+    const raytrace = canvas.raytrace;
+
+    const now = performance.timeOrigin + performance.now();
+
+    gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+    gl.useProgram(raytrace.program);
+
+    const speed = [
+        Math.sin(wrappedNumber(now, 1402) * Math.PI),
+        Math.sin(wrappedNumber(now, 3000) * Math.PI),
+        Math.sin(wrappedNumber(now, 2193) * Math.PI),
+        Math.sin(wrappedNumber(now, 9107) * Math.PI),
+        Math.sin(wrappedNumber(now, 5571) * Math.PI)
+    ];
+
+    raytrace.program.uniform.screenSize.setUniformValue(canvas.width, canvas.height);
+
+    gl.bindVertexArray(raytrace.vao);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, raytrace.spheres);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA32F, 3, 6, 0, gl.RGBA, gl.FLOAT, Float32Array.from([
+         90, bounce(speed[1], 100, 320), 100, 50, 0.2, 0.7, 0.5, 64, 0.0, 0.0, 1.0, 1,
+        210, bounce(speed[2], 50, 270), 300, 50, 0.2, 0.3, 0.5,  8, 0.0, 1.0, 0.0, 1,
+        290, bounce(speed[4], 50, 170), 150, 50, 0.2, 0.7, 0.8, 32, 1.0, 0.0, 0.0, 1,
+        140, bounce(speed[3], 50, 220), 400, 50, 0.2, 0.8, 0.5, 32, 1.0, 0.8, 0.0, 1,
+        110, bounce(speed[0], 50, 130), 200, 50, 0.2, 0.8, 0.0,  1, 1.0, 0.5, 0.0, 1,
+        200, 200,                     -1000,1000,0.2, 0.8, 0.1,  4, 0.4, 0.4, 0.4, 1
+    ]));
+
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, raytrace.lights);
+
+    raytrace.program.uniform.spheres.setUniformValue(0);
+    raytrace.program.uniform.lights.setUniformValue(1);
+
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_BYTE, 0);
+
+    gl.bindVertexArray(null);
+
+    gl.useProgram(null);
+
+    gl.finish();
+
+//     const duration = (performance.now() - now) * 1000;
+
+//     console.log(`Frame time: ${duration.toFixed(0)}ms`);
+
+    update();
+}
+
+function onResize() {
+    const canvas = document.getElementById('screen');
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    const gl = canvas.getContext('webgl2');
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    update();
+}
+
+function createSphereTexture() {
+    const canvas = document.getElementById('screen');
+    const gl = canvas.getContext('webgl2');
+    const raytrace = canvas.raytrace = canvas.raytrace ?? {};
+    raytrace.spheres = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, raytrace.spheres);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA32F, 3, 6, 0, gl.RGBA, gl.FLOAT, Float32Array.from([
+         90, 320, 100, 50, 0.2, 0.7, 0.5, 64, 0.0, 0.0, 1.0, 1,
+        210, 270, 300, 50, 0.2, 0.3, 0.5,  8, 0.0, 1.0, 0.0, 1,
+        290, 170, 150, 50, 0.2, 0.7, 0.8, 32, 1.0, 0.0, 0.0, 1,
+        140, 220, 400, 50, 0.2, 0.8, 0.0,  1, 1.0, 0.8, 0.0, 1,
+        110, 130, 200, 50, 0.2, 0.8, 0.5, 32, 1.0, 0.5, 0.0, 1,
+        200, 200,-1000,1000,0.2,0.8, 0.0,  1, 0.4, 0.4, 0.4, 1
+    ]));
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    // This texture must not have mipmaps, texels (texture pixels) are accessed directly.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_BASE_LEVEL, 0);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAX_LEVEL, 0);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+function createLightTexture() {
+    const canvas = document.getElementById('screen');
+    const gl = canvas.getContext('webgl2');
+    const raytrace = canvas.raytrace = canvas.raytrace ?? {};
+    raytrace.lights = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, raytrace.lights);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB32F, 2, 2, 0, gl.RGB, gl.FLOAT, Float32Array.from([
+        -200, +600, 1500, 1.0, 1.0, 1.0,
+         600,  400, 1000, 0.0, 0.2, 0.4
+    ]));
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    // This texture must not have mipmaps, texels (texture pixels) are accessed directly.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_BASE_LEVEL, 0);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAX_LEVEL, 0);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+function createRaytraceSurface() {
+    const pseudoVertices = Float32Array.from([
+        -1, +1,
+        -1, -1,
+        +1, -1,
+        +1, +1
+    ]);
+    const pseudoIndices = Uint8Array.from([
+        // 0 3
+        // 1 2
+        0, 1, 2,
+        0, 2, 3
+    ]);
+    const canvas = document.getElementById('screen');
+    const gl = canvas.getContext('webgl2');
+
+    gl.clearColor(0, 0, 0, 0);
+    // gl.enable(gl.BLEND);
+    // gl.blendFunc(gl.ZERO, gl.DST_ALPHA);
+
+    const raytrace = canvas.raytrace = canvas.raytrace ?? {};
+    raytrace.vao = gl.createVertexArray();
+    raytrace.vbo = gl.createBuffer();
+    raytrace.ibo = gl.createBuffer();
+
+    gl.bindVertexArray(raytrace.vao);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, raytrace.vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, pseudoVertices, gl.STATIC_DRAW);
+
+    gl.enableVertexAttribArray(0);
+    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 2 * 4, 0);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, raytrace.ibo);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, pseudoIndices, gl.STATIC_DRAW);
+
+    gl.bindVertexArray(null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+}
+
+async function fetchTextFile(url) {
+    const response = await fetch(url);
+    if(!response.ok) {
+        const error = new Error(`Unable to fetch file: ${url}`);
+        error.response = response;
+        error.url = url;
+        throw error;
+    }
+    return response.text();
+}
+
+async function main() {
+    const canvas = document.getElementById('screen');
+    const gl = canvas.getContext('webgl2');
+    const raytrace = canvas.raytrace = canvas.raytrace ?? {};
+    const sources = {
+        vertex: await fetchTextFile(new URL('vertex.glsl', import.meta.url)),
+        fragment: await fetchTextFile(new URL('fragment.glsl', import.meta.url))
+    }
+    raytrace.program = createProgram(gl, sources.vertex, sources.fragment);
+    createRaytraceSurface();
+    createSphereTexture();
+    createLightTexture();
+    window.addEventListener('resize', onResize);
+    onResize();
+}
+
+function freeResources() {
+    const canvas = document.getElementById('screen');
+    const gl = canvas.getContext('webgl2');
+    const raytrace = canvas.raytrace = canvas.raytrace ?? {};
+
+    if (gl.isBuffer(raytrace.vbo)) {
+        gl.deleteBuffer(raytrace.vbo);
+    }
+    if (gl.isBuffer(raytrace.ibo)) {
+        gl.deleteBuffer(raytrace.ibo);
+    }
+    if (gl.deleteVertexArray(raytrace.vao)) {
+        gl.deleteVertexArray(raytrace.vao);
+    }
+    if (gl.isProgram(raytrace.program)) {
+        for (const shader of gl.getAttachedShaders(raytrace.program)) {
+            if (gl.isShader(shader)) {
+                gl.deleteShader(shader);
+            }
+        }
+        gl.deleteProgram(raytrace.program);
+    }
+    if (gl.isTexture(raytrace.spheres)) {
+        gl.deleteTexture(raytrace.spheres);
+    }
+    if (gl.isTexture(raytrace.lights)) {
+        gl.deleteTexture(raytrace.lights);
+    }
+}
+
+function runMain() {
+    main().catch(error => {
+        console.error(error);
+    });
+}
+
+// When using modules there is a possibility for global await in some import.
+// In such case this code might be reached after the "load" event fires.
+// Unfortunately, the "window" object does not keep a state, but the "document"
+// does keep a state, but we must use "readystatechange" API.
+if (document.readyState !== 'complete') {
+    const onReadyStateChange = function (event) {
+        if (document.readyState !== 'loading') {
+            document.removeEventListener('readystatechange', onReadyStateChange);
+            window.removeEventListener('load', onReadyStateChange);
+            setTimeout(runMain, 0);
+        }
+    };
+    document.addEventListener('readystatechange', onReadyStateChange, { passive: true });
+    // In case "readystatechange" is not available, this is the best we can do.
+    window.addEventListener('load', onReadyStateChange, { once: true, passive: true });
+} else {
+    setTimeout(runMain, 0);
+}
+
+window.addEventListener('unload', freeResources, {
+    once: true,
+    passive: true
+});
