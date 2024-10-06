0c1dd8854eaa27b715a9249ecce7b04e7b7f7e7a
Plamen Dragiyski <plamen@dragiyski.org>
2024-10-06 22:43:06 +0200

    vault: add manifest.json to the index.html

--- vault/assets/browserAll-BsAnbkrR.js
+++
@@ -1,2 +0,0 @@
-import{e as i,A as e,C as a,a as s,E as t,F as n}from"./main-AVFmBMGc.js";import"./webworkerAll-Ct7BIAnK.js";i.add(e);a.mixin(s);i.add(t);a.mixin(n);
-//# sourceMappingURL=browserAll-BsAnbkrR.js.map
