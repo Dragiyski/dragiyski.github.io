58a5a4375af54fe4642a9846bfa55aa19fff9c36
Plamen Dragiyski <plamen@dragiyski.org>
2024-10-06 22:34:01 +0200

    vault: fix relative url to document root relative URLs

---
+++ vault/assets/browserAll-BOEnvqdA.js
@@ -0,0 +1,2 @@
+import{e as i,A as e,C as a,a as s,E as t,F as n}from"./main-BjTk1TDQ.js";import"./webworkerAll-BGC1F2Vi.js";i.add(e);a.mixin(s);i.add(t);a.mixin(n);
+//# sourceMappingURL=browserAll-BOEnvqdA.js.map
