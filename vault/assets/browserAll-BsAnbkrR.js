1ae3117feee8a374311d61572ae9f78b09d4739c
Plamen Dragiyski <plamen@dragiyski.org>
2024-10-06 22:27:31 +0200

    add vault game for testing purposes

---
+++ vault/assets/browserAll-BsAnbkrR.js
@@ -0,0 +1,2 @@
+import{e as i,A as e,C as a,a as s,E as t,F as n}from"./main-AVFmBMGc.js";import"./webworkerAll-Ct7BIAnK.js";i.add(e);a.mixin(s);i.add(t);a.mixin(n);
+//# sourceMappingURL=browserAll-BsAnbkrR.js.map
