35478094006b1df10def7ee8e307f745cf007129
Plamen Dragiyski <plamen@dragiyski.org>
2024-10-07 13:34:21 +0200

    vault: update the spark to match the preview

---
+++ vault/assets/browserAll-DYCX7ysA.js
@@ -0,0 +1,2 @@
+import{e as i,A as e,C as a,a as s,E as t,F as n}from"./main-DCbPfIKA.js";import"./webworkerAll-CyA4MyuN.js";i.add(e);a.mixin(s);i.add(t);a.mixin(n);
+//# sourceMappingURL=browserAll-DYCX7ysA.js.map
